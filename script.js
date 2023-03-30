472a0889195964ba449e6862274b408709d12a17
Shubham Lolusare <shubhamlolusare@gmail.com>
2023-03-30 10:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+let opt = document.getElementById("output");
+
+window.promises = [new Promise((resolve)=>{resolve(Math.floor(Math.random() * (5 - 1) + 1))}),
+				  new Promise((resolve)=>{resolve(Math.floor(Math.random() * (5 - 1) + 1))}),
+				  new Promise((resolve)=>{resolve(Math.floor(Math.random() * (5 - 1) + 1))})];
+
+
+Promise.any(window.promises).then((data)=>{
+	let text = document.createTextNode(data);
+	opt.append(text);
+})
